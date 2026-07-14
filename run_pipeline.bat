ee36eee6e8c5b09ca77c970ad9e8d5faa7b025e4
ethanpelser <96677359+ethanpelser@users.noreply.github.com>
2026-07-14 12:24:49 +0200

    changed run_pipeline.bat to include logging
    I want to know if it works :)
    
    also added the log file to .gitignore

--- run_pipeline.bat
+++ run_pipeline.bat
@@ -2,12 +2,21 @@
 
 cd /d "C:\Users\ethan\portfolio\sql _Normalisations_project\crypto-incremental-etl-pipeline"
 
-"venv\Scripts\python.exe" src\pipeline.py
+echo ============================================== >> task_scheduler.log
+echo Started: %date% %time% >> task_scheduler.log
 
-git add crypto_prices.db
+venv\Scripts\python.exe src\pipeline.py >> task_scheduler.log 2>&1
+
+echo Pipeline exit code: %errorlevel% >> task_scheduler.log
+
+git add crypto_prices.db >> task_scheduler.log 2>&1
 
 git diff --cached --quiet
 if errorlevel 1 (
-    git commit -m "data: scheduled database update"
-    git push origin main
+    git commit -m "data: scheduled database update" >> task_scheduler.log 2>&1
+    git push origin main >> task_scheduler.log 2>&1
+) else (
+    echo No database changes to commit. >> task_scheduler.log
 )
+
+echo Finished: %date% %time% >> task_scheduler.log
